c9ff4a9408faa4b56d70349a0f22996d630d8fe7
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-11-22 18:23:51 +0600

    sidebar design improve

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-50 h-screen pt-20 transition-transform -translate-x-full bg-orange-100 border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-52 h-screen pt-20 transition-transform -translate-x-full bg-orange-100 border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-orange-100 dark:bg-gray-800">
        <ul class="space-y-2 font-medium">
           <li>
