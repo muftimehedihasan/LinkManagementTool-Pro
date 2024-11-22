7ea118fda3979c629b287fd30d4915df95e577c1
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-11-23 02:37:30 +0600

    improve logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -169,7 +169,7 @@
                 </button>
                 <a href="{{ route('dashboard') }}" class="flex ms-2 md:me-24">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-                    <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Link MN</span>
+                    <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Link mn</span>
                 </a>
             </div>
 
