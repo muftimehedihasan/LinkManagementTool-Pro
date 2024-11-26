5950ecbae35fdc59cd9b61a5f98d788823f7e564
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-11-26 12:32:33 +0600

    add factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,14 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\User::factory(10) // Create 10 users
+            ->has(\App\Models\Link::factory(10)) // Each user gets 10 links
+            ->create();
     }
 }
