f4f556a07ce97555a59fa9354ed2bc19ea08482d
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-11-28 17:57:30 +0600

    design update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Link mn</span>
         </a>
 
-        <div id="mega-menu" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+        {{-- <div id="mega-menu" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
             <ul class="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
                 <li>
                     <a href="#" class="block py-2 px-3 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
@@ -44,7 +44,7 @@
                     <a href="#" class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                 </li>
             </ul>
-        </div>
+        </div> --}}
 
 
         @if (Route::has('login'))
